4b4e8b6ab5e915b005fcdd15bc79df86d37767a8
Hugo Santos <hugosantos.ufg@gmail.com>
2025-07-02 18:10:36 -0300

    correcoes cors

--- src/main/java/br/com/petzon/petzonapi/config/WebSocketConfig.java
+++ src/main/java/br/com/petzon/petzonapi/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package br.com.petzon.petzonapi.config;
 
 import br.com.petzon.petzonapi.security.WebSocketAuthInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -16,9 +17,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketAuthInterceptor webSocketAuthInterceptor;
 
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins(allowedOrigins)
+                .withSockJS();
     }
 
     @Override
